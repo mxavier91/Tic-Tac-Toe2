ac43b631a89a599c9a7d55e0145045f8d7823399
mxavier91 <mxavier17@gmail.com>
2018-02-11 15:14:40 -0500

    Making Progress
    Made changes to JS slowly and methodically making Changes

--- assets/scripts/game-logic.js
+++ assets/scripts/game-logic.js
@@ -1,10 +1,11 @@
 let origBoard
-const playerOne = 'O'
-const playerTwo = 'X'
+const playerOne = 'X'
+const playerTwo = 'O'
 const winCombos = [
   [0, 1, 2],
   [3, 4, 5],
   [6, 7, 8],
+  [0, 3, 6],
   [1, 4, 7],
   [2, 5, 8],
   [0, 4, 8],
@@ -15,12 +16,14 @@ const cells = $('.cell')
 
 const startGame = function () {
   origBoard = Array.from(Array(9).keys())
+  for (let i = 0; i < cells.length; i++) {
+    cells[i].innerText = ''
+    cells[i].addEventListner('click', turnClick)
+  }
 }
 
-startGame()
-
-const turnClick = function () {
-  console.log('FINALLY!!!!')
+const turnClick = function (square) {
+  console.log(square.target.id)
 }
 
 module.exports = {
