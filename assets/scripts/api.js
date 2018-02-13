c0665e4d85bb2263fe84f1c36717d9143c51ad7b
mxavier91 <mxavier17@gmail.com>
2018-02-13 11:18:35 -0500

    Finally got sign in and sign up buttons to work
    With lots of help I finally got the buttons to work

--- assets/scripts/api.js
+++ assets/scripts/api.js
@@ -13,6 +13,18 @@ const signUp = function (data) {
   })
 }
 
+const signIn = function (data) {
+  return $.ajax({
+    url: config.apiOrigin + '/sign-in',
+    method: 'POST',
+    headers: {
+      contentType: 'application/json'
+    },
+    data: data
+  })
+}
+
 module.exports = {
-  signUp
+  signUp,
+  signIn
 }
--- assets/scripts/ui.js
+++ assets/scripts/ui.js
@@ -5,12 +5,26 @@ const signUpSuccess = function (data) {
 }
 
 const signUpFailure = function (data) {
-  $('#message').text('Signed in Successfully')
+  $('#message').text('Signed up Successfully')
   $('#message').css('background-color', 'red')
+  console.log(data)
+}
+
+const signInSuccess = function (data) {
+  $('#message').text('Signed in Successfully')
+  $('#message').css('background-color', 'blue')
+  console.log(data)
+}
+
+const signInFailure = function (data) {
+  $('#message').text('Signed in Successfully')
+  $('#message').css('background-color', 'yellow')
   console.log(data)
 }
 
 module.exports = {
   signUpSuccess,
-  signUpFailure
+  signUpFailure,
+  signInSuccess,
+  signInFailure
 }
